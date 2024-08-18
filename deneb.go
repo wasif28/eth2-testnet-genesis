58b24d01f8a48ffac82ba9b7e82386ec5e6627f2
Sheikh Wasif <wasif28@hotmail.com>
2024-08-19 03:03:11 +0500

    Update deneb.go

--- deneb.go
+++ deneb.go
@@ -67,6 +67,7 @@ func (g *DenebGenesisCmd) Default() {
 
 func (g *DenebGenesisCmd) Run(ctx context.Context, args ...string) error {
 	fmt.Printf("zrnt version: %s\n", eth2.VERSION)
+	fmt.Printf("effectivebalance %s\n", g.EffectiveBalance)
 
 	spec, err := g.SpecOptions.Spec()
 	if err != nil {
